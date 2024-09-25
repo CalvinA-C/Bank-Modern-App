edfae427c83c30c7a304c922f5610a6099874c22
CalvinA-C <calvinalex.currie@gmail.com>
2024-09-25 15:43:14 +1000

    bank_modern_app--cardDeal-section-completed

--- bank_modern_app/src/components/CardDeal.jsx
+++ bank_modern_app/src/components/CardDeal.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
 const CardDeal = () => {
-  return <div></div>;
+  return (
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Find a better card deal <br className="sm:block hidden" />
+          in few easy steps.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt5`}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto,
+          fuga consequuntur pariatur recusandae necessitatibus quibusdam quaerat
+          quam eveniet rerum dicta.
+        </p>
+        <Button styles="mt-10" />
+      </div>
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card" className="w-[100%] h-[100%]" />
+      </div>
+    </section>
+  );
 };
 
 export default CardDeal;
